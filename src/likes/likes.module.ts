5548a6176686e8c3a18940510d3e09ea6dcc39cf
AJ <81845474+ajfuturistics@users.noreply.github.com>
2026-01-04 22:32:41 +0530

    added forein keys and added user guard validation

--- src/likes/likes.module.ts
+++ src/likes/likes.module.ts
@@ -4,11 +4,13 @@ import { Like } from './entities/like.entity';
 import { LikesService } from './services/likes.service';
 import { LikesController } from './controllers/likes.controller';
 import { PostsModule } from '../posts/posts.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([Like]),
         forwardRef(() => PostsModule),
+        UsersModule,
     ],
     controllers: [LikesController],
     providers: [LikesService],
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -4,11 +4,13 @@ import { Post } from './entities/post.entity';
 import { PostsService } from './services/posts.service';
 import { PostsController } from './controllers/posts.controller';
 import { LikesModule } from '../likes/likes.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([Post]),
         forwardRef(() => LikesModule),
+        UsersModule,
     ],
     controllers: [PostsController],
     providers: [PostsService],
